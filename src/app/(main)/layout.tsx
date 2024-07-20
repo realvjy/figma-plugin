09df1eb7c5867fc8d269770f44749c2c86bb6226
realvjy <realvjy@gmail.com>
2024-07-20 17:03:10 +0530

    overflow fix

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -20,6 +20,29 @@ export default function Layout({ children }: { children: ReactNode }) {
 
 const Main = styled.main`
   position: relative;
+  height: 100vh;
+  overflow: hidden;
+  &::before {
+    content: "";
+    position: absolute;
+    inset: 0;
+    z-index: -100;
+    left: 50%;
+    top: -50px;
+    transform: translate(-50%, -50%);
+    height: 350px;
+    width: 800px;
+    mix-blend-mode: color-dodge;
+    border-radius: 50%;
+    filter: blur(62px);
+    background: linear-gradient(
+      0deg,
+      rgba(25, 0, 255, 0.2) 0%,
+      rgba(255, 0, 0, 1) 100%
+    );
+    z-index: -1;
+    pointer-events: none;
+  }
 `;
 
 const StarWrapper = styled.div`
